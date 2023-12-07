a00c80d07df827d6cc856fa2fc2fae13daf57390
amirrezaakbari <123993165+amirreza-akbari@users.noreply.github.com>
2023-12-07 11:04:58 -0800

    Add files via upload

---
+++ sizeof.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+main()
+{
+	int   a = 123;
+	float b = 10.5;
+	char  c = 'h';
+	bool  d = true;
+	
+	cout << sizeof(a) << endl;
+	cout << sizeof(b) << endl;
+	cout << sizeof(c) << endl;
+	cout << sizeof(d);
+}
