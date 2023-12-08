36c08889c61367d39dbcbdcc99667e537c54e13f
amirrezaakbari <123993165+amirreza-akbari@users.noreply.github.com>
2023-12-08 10:39:25 -0800

    Add files via upload

---
+++ cmath.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+main()
+{
+	cout << sqrt(64) << endl;
+	cout << abs(-64) << endl;
+	cout << pow(2,10) << endl;
+
+	cout << floor(3.6) << endl;
+	cout << ceil(3.6) << endl;
+	cout << trunc(3.6) << endl;
+
+	cout << log10(100) << endl;
+	cout << log2(32) << endl;
+
+	cout << fmod(17,3) << endl;
+
+}
