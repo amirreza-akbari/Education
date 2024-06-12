128d2d4f457fe86879ff0562affc6b8aa606d1e6
amirrezaakbari <123993165+amirreza-akbari@users.noreply.github.com>
2024-06-12 20:44:16 +0330

    Update cmath.cpp

--- cmath.cpp
+++ cmath.cpp
@@ -6,7 +6,7 @@ main()
 	cout << sqrt(64) << endl;
 	cout << abs(-64) << endl;
 	cout << pow(2,10) << endl;
-
+  
 	cout << floor(3.6) << endl;
 	cout << ceil(3.6) << endl;
 	cout << trunc(3.6) << endl;
